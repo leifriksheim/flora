0392997d1bcacf08f4246e1846f52ccce3cd2e51
Leif Riksheim <riksheim.leif@gmail.com>
2022-06-20 15:08:58 +0200

    add temperature and humidity

--- arduino/firebase.ino
+++ arduino/firebase.ino
@@ -45,30 +45,31 @@ void loop()
   if ((WiFi.status() == WL_CONNECTED))
   {
 
-    WiFiClient client;
+    std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
     HTTPClient http;
 
-    float h = 1;
-    float t = 2;
-    float f = 3;
+    client->setInsecure();
+
+    float h = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    float t = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    float f = dht.readTemperature(true);
 
     DynamicJsonDocument doc(1024);
 
-    JsonObject fields = doc.createNestedObject("fields");
-    fields["Name"] = "Mushroom Box 1";
-    fields["Temperature"] = t;
-    fields["Humidity"] = h;
+    doc["Temperature"] = t;
+    doc["Humidity"] = h;
 
     String data;
     serializeJson(doc, data);
 
     Serial.println(data);
 
-    http.begin(client, "https://flora-valldaura.firebaseio.com/readings.json");
+    http.begin(*client, "https://flora-valldaura-default-rtdb.europe-west1.firebasedatabase.app/latest.json");
     http.addHeader("Content-Type", "application/json");
-    http.addHeader("Authorization", "Bearer keyYTxVp7R6EePIrw");
     Serial.println("Sending HTTP Request");
-    int httpResponseCode = http.POST(data);
+    int httpResponseCode = http.PUT(data);
     Serial.println("HTTP Request sent");
     if (httpResponseCode > 0)
     {
@@ -85,6 +86,6 @@ void loop()
 
     http.end();
 
-    delay(10000);
+    delay(20000);
   };
 }
--- main.js
+++ main.js
@@ -1,17 +1,47 @@
 import Spectrogram from "./src/spectogram/index.js";
 import { getObservations } from "./src/inaturalist/index.js";
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/9.8.3/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDY-acv5Cu8dls1EsFZvbJ17-CjEROND3s",
+  authDomain: "flora-valldaura.firebaseapp.com",
+  databaseURL:
+    "https://flora-valldaura-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "flora-valldaura",
+  storageBucket: "flora-valldaura.appspot.com",
+  messagingSenderId: "90774044033",
+  appId: "1:90774044033:web:ea97b282e25e7107d83c80",
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
 import { createApp } from "vue";
 
 createApp({
   data() {
     return {
       isInit: false,
+      temperature: 0,
+      humidity: 0,
     };
   },
   async created() {
     const res = await getObservations();
     console.log(res);
+
+    const readingsRef = ref(database, "latest");
+    onValue(readingsRef, (snapshot) => {
+      const data = snapshot.val();
+      this.temperature = data.temperature;
+      this.humidity = data.humidity;
+    });
   },
   methods: {
     initSpectoGram() {
