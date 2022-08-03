087a183f5ee1846da3f31c74ac45349519d59601
Leif Riksheim <riksheim.leif@gmail.com>
2022-08-03 14:47:18 +0200

    add taxa

--- app/src/routes/Biodiversity/Observations.tsx
+++ app/src/routes/Biodiversity/Observations.tsx
@@ -1,30 +1,47 @@
 import { useEffect, useState, useRef } from "react";
 import "./index.css";
 
-async function getIdentifiers() {
+const nelat = "41.452398072632";
+
+async function getIdentifiers({ taxa = "" }) {
   const res = await fetch(
-    `https://api.inaturalist.org/v1/observations/identifiers?verifiable=true&spam=false&nelat=41.451898597071555&nelng=2.1406042373937506&swlat=41.4474756825558&swlng=2.1290278232854742&locale=nb`
+    `https://api.inaturalist.org/v1/observations/identifiers?verifiable=true&iconic_taxa=${taxa}&spam=false&nelat=41.451898597071555&nelng=2.1406042373937506&swlat=41.4474756825558&swlng=2.1290278232854742&locale=nb`
   );
 
   return res.json();
 }
 
-async function getObservations({ page = 1 }) {
+async function getObservations({ page = 1, taxa = "" }) {
   const res = await fetch(
-    `https://api.inaturalist.org/v1/observations?verifiable=true&order_by=observations.id&order=desc&page=${page}&spam=false&nelat=41.451890555682404&nelng=2.1406096018117804&swlat=41.44746764061841&swlng=2.1290224588674445&locale=nb&per_page=24`
+    `https://api.inaturalist.org/v1/observations?verifiable=true&iconic_taxa=${taxa}&order_by=observations.id&order=desc&page=${page}&spam=false&nelat=41.451890555682404&nelng=2.1406096018117804&swlat=41.44746764061841&swlng=2.1290224588674445&locale=nb&per_page=24`
   );
 
   return res.json();
 }
 
-async function getSpecies({ page = 1 }) {
+async function getSpecies({ page = 1, taxa = "" }) {
   const res = await fetch(
-    `https://api.inaturalist.org/v1/observations/species_counts?verifiable=true&page=${page}&spam=false&&nelat=41.451890555682404&nelng=2.1406096018117804&swlat=41.44746764061841&swlng=2.1290224588674445&locale=nb&per_page=50`
+    `https://api.inaturalist.org/v1/observations/species_counts?verifiable=true&page=${page}&iconic_taxa=${taxa}&spam=false&&nelat=41.451890555682404&nelng=2.1406096018117804&swlat=41.44746764061841&swlng=2.1290224588674445&locale=nb&per_page=50`
   );
   return res.json();
 }
 
 export default function Observations() {
+  const [taxaOptions, setTaxaOptions] = useState([
+    { value: "", label: "All" },
+    { value: "Fungi", label: "Fungi" },
+    { value: "Aves", label: "Birds" },
+    { value: "Amphibia", label: "Amphibia" },
+    { value: "Reptilia", label: "Reptilia" },
+    { value: "Mammalia", label: "Mammalia" },
+    { value: "Mollusca", label: "Mollusca" },
+    { value: "Arachnida", label: "Arachnida" },
+    { value: "Insecta", label: "Insecta" },
+    { value: "Plantae", label: "Plantae" },
+    { value: "Protozoa", label: "Protozoa" },
+    { value: "unknown", label: "Unknown" },
+  ]);
+  const [taxa, setTaxa] = useState("");
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
 
@@ -39,15 +56,17 @@ export default function Observations() {
     const { results: observations, total_results: observationCount } =
       await getObservations({
         page,
+        taxa,
       });
     setObservationCount(observationCount);
     setObservations(observations);
 
-    const { total_results: identifierCount } = await getIdentifiers();
+    const { total_results: identifierCount } = await getIdentifiers({ taxa });
     setIdentifierCount(identifierCount);
 
     const { results: species, total_results: speciesCount } = await getSpecies({
       page,
+      taxa,
     });
     setSpecies(species);
     setSpeciesCount(speciesCount);
@@ -60,6 +79,10 @@ export default function Observations() {
     setPage(page + 1);
     setLoading(false);
   }
+
+  useEffect(() => {
+    fetchData();
+  }, [taxa]);
 
   useEffect(() => {
     fetchData();
@@ -82,6 +105,15 @@ export default function Observations() {
             <small className="info__desc">Identifiers</small>
           </div>
         </div>
+
+        <select className="select" onChange={(e) => setTaxa(e.target.value)}>
+          <option disabled selected>
+            Select species type
+          </option>
+          {taxaOptions.map((option) => (
+            <option value={option.value}>{option.label}</option>
+          ))}
+        </select>
 
         <div className="grid">
           {species.map(({ taxon, count }: any) => {
