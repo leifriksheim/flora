afcd454fce835ed1b0e4c96e7aabda4e8d842220
Leif Riksheim <riksheim.leif@gmail.com>
2022-08-31 11:19:31 +0200

    cleanup

--- src/inaturalist/index.js
+++
@@ -1,7 +0,0 @@
-export async function getObservations() {
-  const res = await fetch(
-    "https://api.inaturalist.org/v1/observations?nelat=41.450501&nelng=2.133909&place_id=any&subview=map&swlat=41.450154&swlng=2.133768"
-  );
-  const json = await res.json();
-  return json;
-}
